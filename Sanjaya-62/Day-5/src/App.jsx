6e63cf3238b84df3c1479a8a0f847e47440ff024
sambitlenka01 <sambitlenkaa@gmail.com>
2026-01-10 22:43:01 +0530

    validation form

---
+++ Sanjaya-62/Day-5/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ValidationForm from './pages/ValidationForm'
+ 
+ const App = () => {
+   return (
+      <ValidationForm/>
+   )
+ }
+ 
+ export default App
---
+++ Sanjaya-62/Day-5/src/pages/ValidationForm.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from 'react'
+import "./Form.css";
+
+const ValidationForm = () => {
+   const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
+
+  const [errors, setErrors] = useState({});
+  const [success, setSuccess] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const validate = () => {
+    let newErrors = {};
+
+    if (!formData.name.trim()) {
+      newErrors.name = "Name is required";
+    }
+
+    if (!formData.email.trim()) {
+      newErrors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      newErrors.email = "Invalid email format";
+    }
+
+    if (!formData.password.trim()) {
+      newErrors.password = "Password is required";
+    } else if (formData.password.length < 6) {
+      newErrors.password = "Password must be at least 6 characters";
+    }
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSuccess(false);
+
+    if (validate()) {
+      setSuccess(true);
+      setFormData({
+        name: "",
+        email: "",
+        password: ""
+      });
+    }
+  };
+  return (
+    <div className="form-container">
+      <h2>Validated Form</h2>
+
+      {success && <div className="success-msg">✅ Submit Successful</div>}
+
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+          {errors.name && <p className="error">{errors.name}</p>}
+        </div>
+
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          {errors.email && <p className="error">{errors.email}</p>}
+        </div>
+
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+          {errors.password && <p className="error">{errors.password}</p>}
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default ValidationForm
